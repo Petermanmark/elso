ac47d65443070e9f8f1b0636e97fc296dd4a61d5
blaiseludvig <blaiseludvig@gmail.com>
2023-08-03 17:31:06 +0200

    change padding to margin on Gyumolcsok

--- src/app/gyumolcsok.tsx
+++ src/app/gyumolcsok.tsx
@@ -6,7 +6,7 @@ export interface GyumolcsokProps {
 
 export function Gyumolcsok(props: GyumolcsokProps) {
   return (
-    <div className="p-4 w-96">
+    <div className="m-4 w-96">
       <ul className="border-2 border-black">
         {props.data.map((gyumolcs) => (
           <Gyumolcs name={gyumolcs.name} isRed={gyumolcs.isRed} />
